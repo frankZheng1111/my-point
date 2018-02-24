1590f0048cab4a008fa3b9a4cdc7d2d00e6312b9
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-02-24 17:25:02 +0800

    Update hello.go

--- hello.go
+++ hello.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	// comment1
+	fmt.Println("Hello, World!")
+	/*
+	 * comment2
+	 * comment3
+	 */
+	fmt.Println("Hello, World! Again!")
 }
