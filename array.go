92c2436387810a458f06e6af3efaa716d739d998
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-02-26 14:56:39 +0800

    use gofmt

--- array.go
+++ array.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-  var n = [10] int { 999, 998 } /* n 是一个长度为 10 的数组 */
+	var n = [10]int{999, 998} /* n 是一个长度为 10 的数组 */
 
-  /* 为数组 n 初始化元素 */         
-  for i := 3; i < 10; i++ {
-    n[i] = i + 100 /* 设置元素为 i + 100 */
-  }
+	/* 为数组 n 初始化元素 */
+	for i := 3; i < 10; i++ {
+		n[i] = i + 100 /* 设置元素为 i + 100 */
+	}
 
-  /* 输出每个数组元素的值 */
-  for j := 0; j < 10; j++ {
-    fmt.Printf("Element[%d] = %d\n", j, n[j] )
-  }
+	/* 输出每个数组元素的值 */
+	for j := 0; j < 10; j++ {
+		fmt.Printf("Element[%d] = %d\n", j, n[j])
+	}
 }
--- function.go
+++ function.go
@@ -3,32 +3,32 @@ package main
 import "fmt"
 
 func main() {
-  /* 定义局部变量 */
-  a := 100
-  b := 200
+	/* 定义局部变量 */
+	a := 100
+	b := 200
 
-  /* 调用函数并返回最大值 */
-  maxValue := max(a, b)
+	/* 调用函数并返回最大值 */
+	maxValue := max(a, b)
 
-  fmt.Println( "最大值是 : %d", maxValue )
-  c, d := swapNumber(a, b)
-  fmt.Println( "输入a = %d, 输入b = %d, 输出 a = %d, b = %d", a, b, c, d )
+	fmt.Println("最大值是 : %d", maxValue)
+	c, d := swapNumber(a, b)
+	fmt.Println("输入a = %d, 输入b = %d, 输出 a = %d, b = %d", a, b, c, d)
 }
 
 /* 函数返回两个数的最大值 */
 func max(num1, num2 int) int {
-  /* 定义局部变量 */
-  var result int
+	/* 定义局部变量 */
+	var result int
 
-  if (num1 > num2) {
-    result = num1
-  } else {
-    result = num2
-  }
-  return result 
+	if num1 > num2 {
+		result = num1
+	} else {
+		result = num2
+	}
+	return result
 }
 
 /* 函数返回两个数的最大值 */
 func swapNumber(num1, num2 int) (int, int) {
-  return num2, num1
+	return num2, num1
 }
