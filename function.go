e7824af784e298700663aae61de0e933a5457c46
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-02-26 14:31:18 +0800

    Add function go

---
+++ function.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+  /* 定义局部变量 */
+  a := 100
+  b := 200
+
+  /* 调用函数并返回最大值 */
+  maxValue := max(a, b)
+
+  fmt.Printf( "最大值是 : %d", maxValue )
+  c, d := swapNumber(a, b)
+  fmt.Printf( "输入a = %d, 输入b = %d, 输出 a = %d, b = %d", a, b, c, d )
+}
+
+/* 函数返回两个数的最大值 */
+func max(num1, num2 int) int {
+  /* 定义局部变量 */
+  var result int
+
+  if (num1 > num2) {
+    result = num1
+  } else {
+    result = num2
+  }
+  return result 
+}
+
+/* 函数返回两个数的最大值 */
+func swapNumber(num1, num2 int) (int, int) {
+  return num2, num1
+}
