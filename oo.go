49ea3be8da82f9cd6b6b4f764ab4f3abdc040e7c
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-03-06 10:40:29 +0800

    minor change

--- oo.go
+++ oo.go
@@ -3,23 +3,23 @@ package main
 import "fmt"
 
 type Robot struct {
-  robot_type string
+	robot_type string
 }
 
 func (robot Robot) showType() {
-  fmt.Println("Robot type is", robot.robot_type)
+	fmt.Println("Robot type is", robot.robot_type)
 }
 
 // 不使用指针则无法修改对象的值
 //
-func (robot *Robot) setType(robot_type string) string{
-  robot.robot_type = robot_type
-  return robot_type
+func (robot *Robot) setType(robot_type string) string {
+	robot.robot_type = robot_type
+	return robot_type
 }
 
 func main() {
-  fmt.Println("Init object")
-  var rx78 Robot
-  rx78.setType("MS")
-  rx78.showType()
+	fmt.Println("Init object")
+	var rx78 Robot
+	rx78.setType("MS")
+	rx78.showType()
 }
