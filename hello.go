5ce4178436797a01a6b4083174091045dc8f6f88
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-03-08 16:35:43 +0800

    update hello.go

--- hello.go
+++ hello.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func f1(a int) (result string, errorMsg string) {
+	if a > 0 {
+		return "ok", ""
+	}
+	return "fail", "a <= 0"
+}
+
 func main() {
 	// comment1
 	fmt.Println("Hello, World!")
@@ -10,4 +17,9 @@ func main() {
 	 * comment3
 	 */
 	fmt.Println("Hello, World! Again!")
+	ret, err := f1(1)
+	fmt.Printf("ret is %s, err is %s\n", ret, err)
+	// 左边至少一个未定义的变量
+	ret2, err := f1(0)
+	fmt.Printf("ret2 is %s, err is %s\n", ret2, err)
 }
