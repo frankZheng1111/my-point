a5fd9cc407218a1dfbc20d811eacd0c907516e62
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-05-08 16:23:23 +0800

    Add scope.go

---
+++ scope.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var mainProfile string
+
+func main() {
+  subProfile := "sub"
+  if condition := "condition"; 3 == (2 + 1) {
+    fmt.Println("conditionPrint: ", condition);
+  }
+  fmt.Println("subPrint ", subProfile);
+}
