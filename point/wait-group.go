efd35047a7f149a1edd031dc6b24ff87c92e38cd
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-05-11 21:00:01 +0800

    Add waitgroup

---
+++ point/wait-group.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "fmt"
+  "runtime"
+  "sync"
+)
+
+func main() {
+  runtime.GOMAXPROCS(1)
+  wg := sync.WaitGroup{}
+  wg.Add(20)
+  for i := 0; i < 10; i++ {
+    go func() {
+      fmt.Println("i: ", i)
+      wg.Done()
+    }()
+  }
+  for i := 0; i < 10; i++ {
+    go func(i int) {
+      fmt.Println("i2: ", i)
+      wg.Done()
+    }(i)
+  }
+  wg.Wait()
+}
+
+// 将GOMAXPROCS设置为1，将影响goroutine的并发，后续代码中的go func()相当于串行执行。
+//
+// 两个for循环内部go func 调用参数i的方式是不同的，后者闭包，导致结果完全不同。这也是新手容易遇到的坑。
+//
+// 第一个go func中i是外部for的一个变量，地址不变化。遍历完成后，最终i=10。故go func执行时，i的值始终是10（10次遍历很快完成）。
+//
+// 第二个go func中i是函数参数，与外部for中的i完全是两个变量。尾部(i)将发生值拷贝，go func内部指向值拷贝地址。
