304d9114e5c2bf239d9452af0615364d926577f8
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-05-15 20:59:47 +0800

    Add quicksort

---
+++ algorithm/quickStart.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+)
+
+func quickSort(values []int) []int {
+  if len(values) <= 1 { return values }
+  baseVal := values[0]
+  var sortValues []int
+  var smallValues []int
+  var bigValues []int
+  for num, value := range values {
+    if num == 0 { continue }
+    if value >= baseVal {
+      bigValues = append(bigValues, value)
+    } else {
+      smallValues = append(smallValues, value)
+    }
+  }
+  if len(smallValues) > 0 { sortValues = append(sortValues, quickSort(smallValues)...) }
+  sortValues = append(sortValues, baseVal)
+  if len(bigValues) > 0 { sortValues = append(sortValues, quickSort(bigValues)...) }
+  return sortValues
+}
+
+func main() {
+  fmt.Println(quickSort([]int {3,2,3,4,3}))
+}
+
