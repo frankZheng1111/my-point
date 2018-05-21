8536f1d63f311379cecf48198f5f18f5e6559ceb
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-05-21 15:23:24 +0800

    Update type change

--- basic/json.go
+++ basic/json.go
@@ -20,10 +20,14 @@ func main() {
 		fmt.Println("error:", err)
 		return
 	}
-	// 尽管 status 字段没有小数点，我们希望它是整数值，解析的时候它还是会被解析成 float64，如果直接把它当做 int 访问，会出现错误：
+	// 尽管 status 字段没有小数点，我们希望它是整数status值，解析的时候它还是会被解析成 float64，如果直接把它当做 int 访问，会出现错误：
+	// 个人理解, 以.(int)为例，完成了查询是否实现了int方法并将interface类型转换为int类型
+	// 若返回俩参数， 则第二个个参数返回判断结果，否则当判断结果是否是引发panic
 	//
-	// var status = result["status"].(int) //error
-	// fmt.Println("status value:", status)
+	// var status, ok = result["status"].(int) // ok is false
+	// var status = result["status"].(int) // will panic: interface conversion: interface {} is float64, not int
+	var status = result["status"].(float64)
+	fmt.Println("status value:", status)
 
 	var userJson = []byte(`{"name": "Wang", "is_admin": false, "auth": { "token": "token1" } }`)
 	var user User
--- basic/type-change.go
+++ basic/type-change.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+/*
+Conversions are expressions of the form T(x) where T is a type and x is an expression that can be converted to type T.
+
+...
+
+A non-constant value x can be converted to type T in any of these cases:
+
+x is assignable to T.
+x's type and T have identical underlying types.
+x's type and T are unnamed pointer types and their pointer base types have identical underlying types.
+x's type and T are both integer or floating point types.
+x's type and T are both complex types.
+x is an integer or a slice of bytes or runes and T is a string type.
+x is a string and T is a slice of bytes or runes.
+*/
 func main() {
 	var sum int = 17
 	var count int = 5
