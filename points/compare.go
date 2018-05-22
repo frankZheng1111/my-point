e667766aaf087e3423bdf39400eeec35772e79b2
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-05-22 11:56:10 +0800

    Add points.go

--- points/compare.go
+++ points/compare.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 // You can use the equality operator, ==, to compare struct variables if each structure field can be compared with the equality operator.
 
@@ -28,9 +31,18 @@ type udata struct {
 func main() {
 	v1 := data{}
 	v2 := data{}
-	fmt.Println("v1 == v2:", v1 == v2) // prints: v1 == v2: true
+	fmt.Println("v1 == v2: ", v1 == v2) // prints: v1 == v2: true
 
 	v3 := udata{}
 	v4 := udata{}
-	fmt.Println("v3 == v4:", v3 == v4) // invalid operation: v3 == v4 (struct containing [10]func() bool cannot be compared)
+	// fmt.Println("v3 == v4:", v3 == v4) // invalid operation: v3 == v4 (struct containing [10]func() bool cannot be compared)
+
+	v3.m = map[string]string{"one": "a", "two": "b"}
+	v4.m = map[string]string{"two": "b", "one": "a"}
+	fmt.Println("v3 == v4(with map): ", reflect.DeepEqual(v3, v4)) // true
+
+	v4.bytes = []byte{'a', 'b'}
+	v3.bytes = []byte{'b', 'a'}
+
+	fmt.Println("v3 == v4(with slice): ", reflect.DeepEqual(v3, v4)) // false
 }
