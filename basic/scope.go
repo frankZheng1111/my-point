131afab0de8fdc7365421dada91185bc5ce1b773
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-05-23 16:17:07 +0800

    gofmt

--- basic/scope.go
+++ basic/scope.go
@@ -1,8 +1,8 @@
 package main
 
-import (  
-  "fmt"
-  "time"
+import (
+	"fmt"
+	"time"
 )
 
 var mainProfile string
@@ -14,19 +14,19 @@ func main() {
 	}
 	fmt.Println("condition(main)", condition)
 
-  data := []string{"one","two","three"}
+	data := []string{"one", "two", "three"}
 
-  for _,v := range data {
-    vcopy := v //
-    go func() {
-      fmt.Println("vcopy:", vcopy)
-      fmt.Println("vcopy address:", &vcopy)
-      fmt.Println("v: ", v)
-      fmt.Println("v address ", &v)
-    }()
-  }
+	for _, v := range data {
+		vcopy := v //
+		go func() {
+			fmt.Println("vcopy:", vcopy)
+			fmt.Println("vcopy address:", &vcopy)
+			fmt.Println("v: ", v)
+			fmt.Println("v address ", &v)
+		}()
+	}
 
-  time.Sleep(3 * time.Second)
-  //goroutines vcopy print: one, two, three & diff address
-  //goroutines v print: three, three, three & same address
+	time.Sleep(3 * time.Second)
+	//goroutines vcopy print: one, two, three & diff address
+	//goroutines v print: three, three, three & same address
 }
