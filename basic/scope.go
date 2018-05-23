94be5b4d6823c705b5da215ed0449ac4a8ec52a4
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-05-23 16:09:44 +0800

    update scope

--- basic/scope.go
+++ basic/scope.go
@@ -1,13 +1,32 @@
 package main
 
-import "fmt"
+import (  
+  "fmt"
+  "time"
+)
 
 var mainProfile string
 
 func main() {
-	subProfile := "sub"
-	if condition := "condition"; 3 == (2 + 1) {
+	condition := "main"
+	if condition := "if"; 3 == (2 + 1) {
 		fmt.Println("conditionPrint: ", condition)
 	}
-	fmt.Println("subPrint ", subProfile)
+	fmt.Println("condition(main)", condition)
+
+  data := []string{"one","two","three"}
+
+  for _,v := range data {
+    vcopy := v //
+    go func() {
+      fmt.Println("vcopy:", vcopy)
+      fmt.Println("vcopy address:", &vcopy)
+      fmt.Println("v: ", v)
+      fmt.Println("v address ", &v)
+    }()
+  }
+
+  time.Sleep(3 * time.Second)
+  //goroutines vcopy print: one, two, three & diff address
+  //goroutines v print: three, three, three & same address
 }
