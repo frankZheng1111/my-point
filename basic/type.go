cfbadc49dc7f5bde63a623e186494ba7a207dd2d
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-05-23 15:54:53 +0800

    update type.go

--- basic/type.go
+++ basic/type.go
@@ -1,15 +1,31 @@
 package main
 
-// When you create a type declaration by defining a new type from an existing (non-interface) type,
+// When you create a type declaration by defining a new type from an existing (non-interface) type, 
 // you don't inherit the methods defined for that existing type.
 //
 
 import "sync"
+//
+// type myMutex sync.Mutex
+//
+// func main() {  
+//   var mtx myMutex
+//   mtx.Lock() //error
+//   mtx.Unlock() //error  
+// }
 
-type myMutex sync.Mutex
 
-func main() {
-	var mtx myMutex
-	mtx.Lock()   //error
-	mtx.Unlock() //error
+// If you do need the methods from the original type you can define a new struct type embedding the original type as an anonymous field.
+// Interface type declarations also retain their method sets
+
+type myLocker struct {  
+  sync.Mutex
+}
+
+// type myLocker sync.Locker
+
+func main() {  
+  var lock myLocker
+  lock.Lock() //ok
+  lock.Unlock() //ok
 }
