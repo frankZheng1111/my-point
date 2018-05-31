c71fa6a1bb26ff7a1b18c7772e94073c07be559f
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-05-31 22:41:16 +0800

    update struct relfect

--- basic/struct-tag.go
+++ basic/struct-tag.go
@@ -10,10 +10,18 @@ func main() {
 		F  string `species:"gopher" color:"blue"`
 		F2 string `species:"gopher2" color:"blue2"`
 	}
-	s := S{}
+	s := S{"f1", "f2"}
 	st := reflect.TypeOf(s)
 	field := st.Field(0)
 	field2 := st.Field(1)
 	fmt.Println(field.Tag.Get("color"), field.Tag.Get("species"))
 	fmt.Println(field2.Tag.Get("color"), field2.Tag.Get("species"))
+
+	for m := 0; m < reflect.TypeOf(s).NumField(); m++ {
+		field := reflect.TypeOf(s).Field(m)
+		fmt.Println(field.Type)
+		fmt.Println(field.Name)
+	}
+
+	fmt.Println(reflect.ValueOf(s).FieldByName("F"))
 }
