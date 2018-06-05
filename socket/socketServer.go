9042119195354fc5991e6a24f537fe82fabdfc89
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-06-05 22:05:53 +0800

    Add socket smaple

---
+++ socket/socketServer.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	//建立socket，监听端口
+	// Step1: Listen
+	//
+	netListen, err := net.Listen("tcp", "localhost:1024")
+	CheckError(err)
+	defer netListen.Close()
+
+	Log("Waiting for clients")
+	for {
+
+		// Step2: Accept
+		//
+		conn, err := netListen.Accept()
+		if err != nil {
+			continue
+		}
+
+		Log(conn.RemoteAddr().String(), " tcp connect success")
+		handleConnection(conn)
+	}
+}
+
+//处理连接
+//
+func handleConnection(conn net.Conn) {
+
+	buffer := make([]byte, 2048)
+
+	for {
+
+		// Step3: ReadBuffer
+		//
+		n, err := conn.Read(buffer)
+
+		if err != nil {
+			Log(conn.RemoteAddr().String(), " connection error: ", err)
+			return
+		}
+
+		Log(conn.RemoteAddr().String(), "receive data string:\n", string(buffer[:n]))
+
+	}
+
+}
+func Log(v ...interface{}) {
+	log.Println(v...)
+}
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
