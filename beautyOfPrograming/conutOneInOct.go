6f5d86213c03f805514f658a07ba9affbdfb7418
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-06-10 22:07:42 +0800

    update 2.4

--- beautyOfPrograming/conutOneInOct.go
+++ beautyOfPrograming/conutOneInOct.go
@@ -1,7 +1,6 @@
 package main
 
 import "fmt"
-import "math"
 
 func Count1InInteger(num int) (count int) {
 	for num > 0 {
@@ -21,17 +20,28 @@ func Count1LessIntegerRaw(num int) (count int) {
 }
 
 func Count1LessIntegerByRule(num int) (count int) {
-	x := 0
-	for num > 0 {
-		count += num % 10 * (1 + x*int(math.Pow(10, float64(x-1))))
-		num /= 10
-		x++
+	lowerNum := 0   // 低位
+	currNum := 0    // 当前位
+	higherNum := 0  //高位
+	var baseNum int //当前位数 1为个位，10为十位
+	for baseNum = 1; baseNum <= num; baseNum = baseNum * 10 {
+		lowerNum = num % baseNum
+		higherNum = num / (baseNum * 10)
+		currNum = (num - higherNum*baseNum*10 - lowerNum) / baseNum
+		switch currNum {
+		case 0:
+			count += higherNum * baseNum
+		case 1:
+			count += higherNum*baseNum + lowerNum + 1
+		default:
+			count += (higherNum + 1) * baseNum
+		}
 	}
 	return
 }
 
 func main() {
-	n := 110
+	n := 12345
 	fmt.Printf("1到%v中1出现的次数(遍历): %v\n", n, Count1LessIntegerRaw(n))
 	fmt.Printf("1到%v中1出现的次数(规则): %v\n", n, Count1LessIntegerByRule(n))
 }
