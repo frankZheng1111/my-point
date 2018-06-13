1ca443a5f793e88a14a3688142f42a42a176cefb
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-06-13 18:31:12 +0800

    Add snowflake

---
+++ algorithm/snowflake.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type SnowFlake struct {
+	Sm             sync.Mutex
+	StartTimestamp int64 // 41位
+	CurrentId      int64 // 12位  0 ~ 4095
+	DatabaseId     string
+	WorkerId       string
+	LastTimestamp  string
+}
+
+func FillInBinaryString(bstring string, length int) string {
+	result := bstring
+	for len(result) < length {
+		result = "0" + result
+	}
+	return result
+}
+
+func (snowFlake *SnowFlake) CurrentTimestamp() string {
+	return FillInBinaryString(fmt.Sprintf("%b", time.Now().UnixNano()/1000000-snowFlake.StartTimestamp), 41)
+}
+
+func (snowFlake *SnowFlake) GenerateId() interface{} {
+	IdStr := "0"
+	snowFlake.Sm.Lock()
+	currentTimestamp := snowFlake.CurrentTimestamp()
+	if snowFlake.LastTimestamp != currentTimestamp {
+		snowFlake.LastTimestamp = currentTimestamp
+		snowFlake.CurrentId = 0
+	}
+	IdStr += fmt.Sprintf("%s%s%s%s", currentTimestamp, snowFlake.DatabaseId, snowFlake.WorkerId, FillInBinaryString(strconv.FormatInt(snowFlake.CurrentId, 2), 12))
+	snowFlake.CurrentId++
+	snowFlake.Sm.Unlock()
+	fmt.Println(IdStr)
+	Id, _ := strconv.ParseInt(IdStr, 2, 10)
+	return Id
+}
+
+func main() {
+	snowFlake := new(SnowFlake)
+	snowFlake.StartTimestamp = time.Now().UnixNano() / 1000000
+	snowFlake.DatabaseId = "00000"
+	snowFlake.WorkerId = "00000"
+	for i := 0; i < 50; i++ {
+		fmt.Println(snowFlake.GenerateId())
+	}
+}
