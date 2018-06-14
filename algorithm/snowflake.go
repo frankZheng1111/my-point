83231ce8a4124efb615de23669b3dbe59e78de1e
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-06-14 16:43:34 +0800

    update snowflake

--- algorithm/snowflake.go
+++ algorithm/snowflake.go
@@ -8,16 +8,19 @@ import (
 	"time"
 )
 
-const MAX_CURRENT_ID = 4095
 const TIME_UNIT = 1 * time.Millisecond // 时间戳最小单位
+const CURRENT_ID_MAX_BIT = 12
+const DATA_ID_MAX_BIT = 5
+const WORKER_ID_MAX_BIT = 5
+const MAX_CURRENT_ID = 1<<CURRENT_ID_MAX_BIT - 1
 
 type SnowFlake struct {
 	Sm             sync.Mutex
 	StartTimestamp int64 // 41位
 	CurrentId      int64 // 12位  0 ~ 4095
-	DatabaseId     string
-	WorkerId       string
-	LastTimestamp  string
+	DataCenterId   int64
+	WorkerId       int64
+	LastTimestamp  int64
 }
 
 func FixedLengthBinaryString(num int64, length int) string {
@@ -28,27 +31,28 @@ func FixedLengthBinaryString(num int64, length int) string {
 	return result
 }
 
-func (snowFlake *SnowFlake) CurrentTimestamp() string {
-	return FixedLengthBinaryString(time.Now().UnixNano()/int64(TIME_UNIT/time.Nanosecond)-snowFlake.StartTimestamp, 41)
+func (snowFlake *SnowFlake) CurrentTimestamp() int64 {
+	return time.Now().UnixNano() / int64(TIME_UNIT/time.Nanosecond)
 }
 
 func (snowFlake *SnowFlake) GenerateId() int64 {
-	IdStr := "0"
 	snowFlake.Sm.Lock()
-	currentTimestamp := snowFlake.CurrentTimestamp()
+	currentTimestamp := snowFlake.CurrentTimestamp() - snowFlake.StartTimestamp
 	if snowFlake.LastTimestamp != currentTimestamp {
 		snowFlake.LastTimestamp = currentTimestamp
 		snowFlake.CurrentId = 0
 	}
 	if snowFlake.CurrentId > MAX_CURRENT_ID {
-		snowFlake.Sm.Unlock()
 		time.Sleep(TIME_UNIT)
+		snowFlake.Sm.Unlock()
 		return snowFlake.GenerateId()
 	}
-	IdStr += fmt.Sprintf("%s%s%s%s", currentTimestamp, snowFlake.DatabaseId, snowFlake.WorkerId, FixedLengthBinaryString(snowFlake.CurrentId, 12))
+	Id := currentTimestamp<<(CURRENT_ID_MAX_BIT+WORKER_ID_MAX_BIT+DATA_ID_MAX_BIT) |
+		snowFlake.DataCenterId<<(CURRENT_ID_MAX_BIT+WORKER_ID_MAX_BIT) |
+		snowFlake.WorkerId<<CURRENT_ID_MAX_BIT |
+		snowFlake.CurrentId
 	snowFlake.CurrentId++
 	snowFlake.Sm.Unlock()
-	Id, _ := strconv.ParseInt(IdStr, 2, 64)
 	return Id
 }
 
@@ -56,9 +60,9 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	wg := sync.WaitGroup{}
 	snowFlake := new(SnowFlake)
-	snowFlake.StartTimestamp = time.Now().UnixNano() / int64(TIME_UNIT/time.Nanosecond)
-	snowFlake.DatabaseId = "00000"
-	snowFlake.WorkerId = "00000"
+	snowFlake.StartTimestamp = snowFlake.CurrentTimestamp()
+	snowFlake.DataCenterId = 0
+	snowFlake.WorkerId = 0
 	wg.Add(5000)
 	for i := 0; i < 5000; i++ {
 		go func() {
