56f69a16a43190dd63742ebe520be701dfa31249
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-06-22 14:44:40 +0800

    minor change

--- golang/basic/type-assertion.go
+++ golang/basic/type-assertion.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var data interface{} = "great"
 
-	if res, ok := data.(bool); ok {
+	if res, ok := data.(bool); ok { // 如果只收一个参数断言, 则断言失败时会panic
 		fmt.Println("[is an int] data =>", data, " res => ", res)
 	} else {
 		fmt.Println("[not an int] value =>", data, "res => ", res)
--- golang/basic/typeChange.go
+++ golang/basic/typeChange.go
@@ -7,18 +7,27 @@
 
 c1: x的值本身就可以赋值给类型T的变量
 c2: 两者有相同的底层类型(如int8 int16 int32 int64)
-c3: x类型和T 都是未命名(未命名的指的是未原生命名，int，int32属于named type, []int, type special， struct 属于unnamedType, 只有unnamed类型可以作为方法的接受者)的指针类型，它们的指针指向的对象类型的底层类型一致
+c3: x类型和T 都是未命名(int，int32, type 定义的类型属于named type, []int,  struct 属于unnamedType, 只有named类型可以作为方法的接受者)的指针类型，它们的指针指向的对象类型的底层类型一致
 c4: 整型和浮点型可以互相转换
 c5: x的类型和T都是复数类型
 c6: x是一个整数(按Ascll码转)或(byte或Rune)的slice可转成字符串
 c7: 上一条逆向转
 ...
+
+ps: 关于named类型 和 unamed类型和Underlying类型
+- int，int32, type 定义的类型属于named type, 只有named类型可以作为方法的接受者
+- []int,  struct{} map[string]int属于unnamedType
+- 两个底层类型相同的变量可以赋值的条件至少有一个不是unamed变量
+
 */
 package main
 
 import "fmt"
 
-func case1() {
+func case4() {
+	// var v32 int32 = 2 << 31
+	// var v8 int8
+	// fmt.Println("v8的值为: ", int8(v32)) // panic overflows
 	// case1
 	var sum int = 1000
 	var count int = 3
@@ -35,11 +44,11 @@ func case6() {
 	fmt.Println("numStr转换成功", numStr)
 }
 
-type T1 *int
-type T2 *int
+type T1 *[]int
+type T2 *[]int
 
 func case3() {
-	var num int = 1
+	var num []int = []int{1}
 	var numPtr1 T1 = &num
 	var numPtr2 T2
 	numPtr2 = T2(numPtr1)
@@ -47,8 +56,8 @@ func case3() {
 }
 
 func main() {
-	case1()
 	case3()
+	case4()
 	case6()
 }
 
