203227a6aa89905fe3f2e9c30629ae2ef5ec8679
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-06-22 14:47:30 +0800

    minor change

--- golang/basic/typeAssertion.go
+++ golang/basic/typeAssertion.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 	var data interface{} = "great"
 
-	if res, ok := data.(bool); ok { // 如果只收一个参数断言, 则断言失败时会panic
+	// 如果只收一个参数断言, 则断言失败时会panic
+	// 必须精准断言, 若是int32 断言int8会失败
+	if res, ok := data.(bool); ok {
 		fmt.Println("[is an int] data =>", data, " res => ", res)
 	} else {
 		fmt.Println("[not an int] value =>", data, "res => ", res)
