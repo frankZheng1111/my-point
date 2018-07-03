9cfe0037eba73050bb80632fe1e59d7913151762
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-07-03 23:05:52 +0800

    WIP

--- golang/basic/gosched.go
+++ golang/basic/gosched.go
@@ -1,33 +1,56 @@
 package main
 
+//https://gocn.vip/article/470
+
 import (
-	"fmt"
+	"log"
 	"runtime"
 	"sync"
+	// "time"
+	"fmt"
 )
 
 var done bool = false
+var num int
 
 func wait(wg *sync.WaitGroup, id int) {
-	for !done {
-		fmt.Println("notdone!", id) // 个人理解，每执行一个函数后分配资源
-		// runtime.Gosched()
+	if id > 0 {
+		log.Println("enter wait func", id)
 	}
+	num += 1 - id
+	num += 2 - id
+	if id > 0 {
+		for i := 0; i < 60000; i++ {
+			fmt.Printf("p")
+		}
+	}
+	num += 3
+	num += 4
+	num += 5
+	log.Println("num in ", id, " : ", num)
+	// for !done {
+	// 	// 个人理解: go关键字生成一个goroutine并竞争资源，若竞争到资源后再该关键字执行完之前不会让出资源
+	// 	// 但是一些特定操作会临时让出资源
+	// 	// log.Printf("notdone! %d\n", id)
+	// 	// time.Sleep(time.Nanosecond)
+	runtime.Gosched()
+	// }
 	wg.Done()
 }
 
 func main() {
 
 	runtime.GOMAXPROCS(1)
-	fmt.Println(runtime.GOMAXPROCS(-1))
+	log.Println(runtime.GOMAXPROCS(-1))
 
-	go func() {
-		done = true
-	}()
+	// go func() {
+	// 	log.Println("setdone!") //
+	// 	done = true
+	// }()
 
 	wg := sync.WaitGroup{}
-	wg.Add(4)
-	for i := 0; i < 4; i++ {
+	wg.Add(2)
+	for i := 0; i < 2; i++ {
 		go wait(&wg, i)
 	}
 
@@ -36,5 +59,5 @@ func main() {
 	// }
 
 	wg.Wait()
-	fmt.Println("done!")
+	log.Println("done!")
 }
