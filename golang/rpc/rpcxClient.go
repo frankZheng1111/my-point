cee9e0d42361400d9147b7608e9514edafd74642
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-07-05 17:03:11 +0800

    Add async call

--- golang/rpc/rpcxClient.go
+++ golang/rpc/rpcxClient.go
@@ -24,12 +24,25 @@ func main() {
 		B: 20,
 	}
 
+	// 同步调用
 	reply := &struct{ C int }{}
 	err := xclient.Call(context.Background(), "Mul", args, reply)
 	if err != nil {
 		log.Fatalf("failed to call: %v", err)
 	}
 
-	log.Printf("%d * %d = %d", args.A, args.B, reply.C)
+	log.Printf("同步调用: %d * %d = %d \n", args.A, args.B, reply.C)
 
+	// 异步调用
+	asyncReply := &struct{ C int }{}
+	call, err := xclient.Go(context.Background(), "Mul", args, asyncReply, nil)
+	if err != nil {
+		log.Fatalf("failed to call: %v", err)
+	}
+	replyCall := <-call.Done
+	if replyCall.Error != nil {
+		log.Fatalf("failed to call: %v", replyCall.Error)
+	} else {
+		log.Printf("异步调用: %d * %d = %d, %v \n", args.A, args.B, asyncReply.C, replyCall)
+	}
 }
