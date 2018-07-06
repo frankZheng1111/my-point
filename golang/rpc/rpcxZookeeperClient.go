9f4e0cef2c8377eb490423d22360d733403656e7
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-07-06 18:15:20 +0800

    Add rpcxzookeeper client & server

---
+++ golang/rpc/rpcxZookeeperClient.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/smallnest/rpcx/client"
+	"github.com/smallnest/rpcx/share"
+	"log"
+)
+
+var (
+	zkAddr   = flag.String("zkAddr", "localhost:2181", "zookeeper address")
+	basePath = flag.String("base", "/rpcx_test", "prefix path")
+)
+
+func main() {
+	flag.Parse()
+
+	d := client.NewZookeeperDiscovery(*basePath, "Arith", []string{*zkAddr}, nil)
+	option := client.DefaultOption
+	xclient := client.NewXClient("Arith", client.Failtry, client.RandomSelect, d, option)
+	defer xclient.Close()
+
+	args := &struct{ A, B int }{
+		A: 10,
+		B: 20,
+	}
+
+	var replyStr string
+	ctx := context.WithValue(context.Background(), share.ReqMetaDataKey, map[string]string{"reqMeta": "FromClient"})
+	ctx = context.WithValue(ctx, share.ResMetaDataKey, make(map[string]string))
+	err := xclient.Call(ctx, "PrintMetaData", "reqStr", &replyStr)
+	if err != nil {
+		log.Fatalf("failed to call: %v", err)
+	}
+
+	log.Printf("同步调用: PrintMetaData: reply: %s, metaData: %s \n", replyStr, ctx.Value(share.ResMetaDataKey).(map[string]string)["resMeta"])
+
+	// 同步调用
+	reply := &struct{ C int }{}
+	err = xclient.Call(context.Background(), "Mul", args, reply)
+	if err != nil {
+		log.Fatalf("failed to call: %v", err)
+	}
+
+	log.Printf("同步调用: %d * %d = %d \n", args.A, args.B, reply.C)
+
+	// 异步调用
+	asyncReply := &struct{ C int }{}
+	call, err := xclient.Go(context.Background(), "Mul", args, asyncReply, nil)
+	if err != nil {
+		log.Fatalf("failed to call: %v", err)
+	}
+	replyCall := <-call.Done
+	if replyCall.Error != nil {
+		log.Fatalf("failed to call: %v", replyCall.Error)
+	} else {
+		log.Printf("异步调用: %d * %d = %d, %v \n", args.A, args.B, asyncReply.C, replyCall)
+	}
+}
---
+++ golang/rpc/rpcxZookeeperServer.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	metrics "github.com/rcrowley/go-metrics"
+	"github.com/smallnest/rpcx/server"
+	"github.com/smallnest/rpcx/serverplugin"
+	"github.com/smallnest/rpcx/share"
+	"log"
+	"time"
+)
+
+type Args struct {
+	A int `msg:"a"`
+	B int `msg:"b"`
+}
+
+type Reply struct {
+	C int `msg:"c"`
+}
+
+type Arith int
+
+func (t *Arith) Mul(ctx context.Context, args *Args, reply *Reply) error {
+	reply.C = args.A * args.B
+	fmt.Println(reply.C, " = ", args.A, " * ", args.B)
+	return nil
+}
+
+func (t *Arith) PrintMetaData(ctx context.Context, args string, reply *string) error {
+	*reply = "success"
+	fmt.Println("req.Meta: ", ctx.Value(share.ReqMetaDataKey).(map[string]string)["reqMeta"])
+	resMeta := ctx.Value(share.ResMetaDataKey).(map[string]string)
+	resMeta["resMeta"] = "FromServer"
+	time.Sleep(3 * time.Second)
+	return nil
+}
+
+var (
+	addr     = flag.String("addr", "localhost:8972", "server address")
+	zkAddr   = flag.String("zkAddr", "localhost:2181", "zookeeper address")
+	basePath = flag.String("base", "/rpcx_test", "prefix path")
+)
+
+func main() {
+	flag.Parse()
+
+	s := server.NewServer()
+	addRegistryPlugin(s)
+
+	s.RegisterName("Arith", new(Arith), "")
+	s.Serve("tcp", *addr)
+}
+
+func addRegistryPlugin(s *server.Server) {
+
+	r := &serverplugin.ZooKeeperRegisterPlugin{
+		ServiceAddress:   "tcp@" + *addr,
+		ZooKeeperServers: []string{*zkAddr},
+		BasePath:         *basePath,
+		Metrics:          metrics.NewRegistry(),
+		UpdateInterval:   time.Minute,
+	}
+	err := r.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Plugins.Add(r)
+}
