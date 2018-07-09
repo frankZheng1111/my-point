85fff0adb59fc73d91d18765739803d419088b22
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-07-09 13:15:24 +0800

    Add some doc

--- golang/rpc/netRPCHTTPServer.go
+++ golang/rpc/netRPCHTTPServer.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"net/rpc"
+	"time"
 )
 
 type Args struct {
@@ -19,10 +20,12 @@ type Quotient struct {
 type Arith int
 
 func (t *Arith) Multiply(args *Args, reply *int) error {
+	time.Sleep(5 * time.Second)
 	*reply = args.A * args.B
 	return nil
 }
 func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	time.Sleep(5 * time.Second)
 	if args.B == 0 {
 		return errors.New("divide by zero")
 	}
@@ -35,6 +38,20 @@ func (t *Arith) Divide(args *Args, quo *Quotient) error {
 func main() {
 	arith := new(Arith)
 	rpc.Register(arith)
+	// HandleHTTP registers an HTTP handler for RPC messages to DefaultServer on DefaultRPCPath and a debugging handler on DefaultDebugPath.
+	// 	DefaultRPCPath   = "/_goRPC_"
+	// 	DefaultDebugPath = "/debug/rpc"
+	// It is still necessary to invoke http.Serve(), typically in a go statement.
+	// 自定义Server
+	// func NewServer() *Server
+	// NewServer returns a new Server.
+	//
+	// func (server *Server) Accept(lis net.Listener)
+	// Accept accepts connections on the listener and serves requests for each incoming connection. Accept blocks until the listener returns a non-nil error. The caller typically invokes Accept in a go statement.
+	//
+	// func (server *Server) HandleHTTP(rpcPath, debugPath string)
+	// HandleHTTP registers an HTTP handler for RPC messages on rpcPath, and a debugging handler on debugPath. It is still necessary to invoke http.Serve(), typically in a go statement.
+	// 可以同时处理多个请求
 	rpc.HandleHTTP()
 	e := http.ListenAndServe(":1234", nil)
 	if e != nil {
