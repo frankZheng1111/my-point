398f7d4c218ed4d7052d10dcafab6d6bb33b5439
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-07-20 12:22:15 +0800

    minor change

--- golang/points/slice.go
+++ golang/points/slice.go
@@ -8,7 +8,9 @@ func FuncSlice(s []int, t int) {
 	s[0]++
 }
 func main() {
+	// a := make([]int, 0, 8)
+	// a = append(a, []int{0, 1, 2, 3}...)
 	a := []int{0, 1, 2, 3}
 	FuncSlice(a, 4)
-	fmt.Println(a) // {1,1,2,3}
+	fmt.Println(a) // {1,1,2,3} // 若是未扩容则是{2，1，2，3}
 }
