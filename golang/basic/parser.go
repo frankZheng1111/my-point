ef63be816a9597200cffb3c998fc0fa3a332c519
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2019-03-26 10:45:39 +0800

    Add some comment

--- golang/basic/parser.go
+++ golang/basic/parser.go
@@ -16,7 +16,20 @@ func main() {
 `
 
 	// Create the AST by parsing src.
+	// A FileSet represents a set of source files. Methods of file sets are synchronized; multiple goroutines may invoke them concurrently.
+	// 多个文件的集合?
 	fset := token.NewFileSet() // positions are relative to fset
+	// func ParseFile(fset *token.FileSet, filename string, src interface{}, mode Mode) (f *ast.File, err error)
+	// type Mode uint
+	// const (
+	// 	PackageClauseOnly Mode             = 1 << iota // stop parsing after package clause
+	// 	ImportsOnly                                    // stop parsing after import declarations
+	// 	ParseComments                                  // parse comments and add them to AST
+	// 	Trace                                          // print a trace of parsed productions
+	// 	DeclarationErrors                              // report declaration errors
+	// 	SpuriousErrors                                 // same as AllErrors, for backward-compatibility
+	// 	AllErrors         = SpuriousErrors             // report all errors (not just the first 10 on different lines)
+	// )
 	f, err := parser.ParseFile(fset, "", src, 0)
 	if err != nil {
 		panic(err)
